4514f7fa09ebc00b976fdfafd93107563aa9a1d0
Ekaterina Vybornykh <nvo28597@educastur.es>
2025-04-07 22:58:26 +0200

    id

--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -41,7 +41,7 @@ function Inicio() {
 
     async function cargarUsuarios() {
         try {
-            let response = await fetch(`${API_URL}/api/usuarios/usuariosCerca/1}`);        //todo cambiar 1 por usuarioLogeado.id
+            let response = await fetch(`${API_URL}/api/usuarios/usuariosCerca/1`);        //todo cambiar 1 por usuarioLogeado.id
 
             if (!response.ok) {
                 throw new Error(`Error en la API: ${response.status} ${response.statusText}`);
