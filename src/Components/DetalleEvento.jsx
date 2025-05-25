faf7dee76beab0dbd97bee3d644276d32806b6aa
Ekaterina Vybornykh <nvo28597@educastur.es>
2025-05-25 15:56:05 +0200

    Antes de instalar lightbox

--- src/Components/DetalleEvento.jsx
+++ src/Components/DetalleEvento.jsx
@@ -3,18 +3,20 @@ import { API_URL } from '../App';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { AppContext } from '../Context/AppProvider';
 import SpinnerWave from './SpinnerWave';
-import { Button, Image, Modal } from 'react-bootstrap';
+import { Button, Form, Image, Modal } from 'react-bootstrap';
 import { RegContext } from '../Context/RegProvider';
 
 function DetalleEvento() {
     const { setEvento, evento } = useContext(AppContext);
     const { usuarioLogueado, token } = useContext(RegContext);
-    const [cargando, setCargando] = useState(true);
+    const [cargando, setCargando] = useState(false);
     const { id } = useParams();
     const [eventoPasado, setEventoPasado] = useState(false);
     const [show, setShow] = useState(false);
     const [mensaje, setMensaje] = useState("");
     const [tipoModal, setTipoModal] = useState("");
+    const [foto, setFoto] = useState("");
+    const [fotos, setFotos] = useState([]);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const navigate = useNavigate();
@@ -29,8 +31,16 @@ function DetalleEvento() {
     );
 
     useEffect(() => {
-        cargarEvento();
-    }, []);
+        if (usuarioLogueado) {
+            cargarEvento();
+        }
+    }, [usuarioLogueado]);
+
+    useEffect(() => {
+        if (usuarioLogueado) {
+            cargarFotos();
+        }
+    }, [usuarioLogueado])
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -56,6 +66,22 @@ function DetalleEvento() {
         setCargando(false);
     }
 
+    async function cargarFotos() {
+        setCargando(true);
+        const response = await fetch(`${API_URL}/imagenes/${id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`Error en la API: ${response.status} ${response.statusText}`);
+        }
+        let data = await response.json();
+        setFotos(data);
+        setCargando(false);
+    }
+
     async function handleParticipar() {
         const response = await fetch(`${API_URL}/participar/${id}/${usuarioLogueado.id}`, {
             method: "POST",
@@ -65,11 +91,13 @@ function DetalleEvento() {
             }
         })
         if (!response.ok) {
+            setTipoModal("error")
             setMensaje("Ha ocurrido un error");
             setShow(true);
         } else {
             const data = await response.json();
             setEvento(data.data);
+            setTipoModal("exito");
             setMensaje("¡Preparate para tu próxima aventura!");
             setShow(true);
         }
@@ -85,10 +113,12 @@ function DetalleEvento() {
         })
         if (!response.ok) {
             setMensaje("Ha ocurrido un error");
+            setTipoModal("error")
             setShow(true);
         } else {
             const data = await response.json();
             setEvento(data.data);
+            setTipoModal("exito");
             setMensaje("Ya no eres participante del evento");
             setShow(true);
         }
@@ -107,6 +137,7 @@ function DetalleEvento() {
             }
         });
         if (!response.ok) {
+            setTipoModal("error")
             setMensaje("Ha ocurrido un error");
             setShow(true);
         } else {
@@ -121,6 +152,53 @@ function DetalleEvento() {
         setTipoModal("");
         navigate('/');
     }
+
+    function handleFotosButton() {
+        setTipoModal("fotos");
+        handleShow();
+    }
+
+    function handleFoto(e) {
+        const file = e.target.files ? e.target.files[0] : null;
+        if (file) {
+            setFoto(file);
+        }
+    }
+
+    const enviarFoto = async () => {
+        if (!foto) {
+            console.log("No hay foto para enviar");
+            return;
+        }
+        const formData = new FormData();
+        if (foto) {
+            formData.append('foto', foto);
+        }
+
+        try {
+            const response = await fetch(`${API_URL}/imagenes/${id}`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+                body: formData,
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                setFotos(data);
+            } else {
+                setTipoModal("error");
+                console.log('Error al subir la foto');
+                setFoto(null);
+            }
+        } catch (error) {
+            console.error('Error al enviar los datos:', error);
+        }
+        setShow(false);
+    };
+
 
     if (cargando) {
         return <div className='container-fluid min-vh-100'>
@@ -141,9 +219,9 @@ function DetalleEvento() {
 
             </div>
             <hr />
-            <div className='row m-5'>
+            <div className='row m-4'>
                 <div className='col-md-4 col-sm-6 col-12'>
-                    <img src={evento.foto_portada} alt="foto" className='w-100 img-fluid rounded' style={{ height: "250px", objectFit: "cover" }}/>
+                    <img src={evento.foto_portada} alt="foto" className='w-100 img-fluid rounded' style={{ height: "250px", objectFit: "cover" }} />
                 </div>
                 <div className='col-sm-2'></div>
                 <div className='col-sm-6'>
@@ -159,8 +237,8 @@ function DetalleEvento() {
                             <Button variant="secondary" className="rounded-pill shadow me-3" onClick={handleEditar}>Editar</Button>
                             <Button variant="secondary" className="rounded-pill shadow" onClick={handleEliminar}>Eliminar</Button>
                         </div>
-                        : eventoPasado
-                            ? ""
+                        : (eventoPasado && (soyParticipante || soyCreador))
+                            ? <Button variant="secondary" className="rounded-pill shadow" onClick={handleFotosButton}>Subir fotos</Button>
                             : soyParticipante
                                 ? <Button variant="secondary" className="rounded-pill shadow" onClick={handleDesapuntarse}>No asistiré</Button>
                                 : sinPlazas
@@ -172,12 +250,27 @@ function DetalleEvento() {
 
                 </div>
             </div>
+            {(eventoPasado && fotos?.length > 0) &&
+                <>
+                    <hr />
+                    <div className='row m-3'>
+                        {
+                            fotos.map((foto, index) => {
+                                return <div key={index} className='col-lg-4 col-sm-6 m-0 p-4'>
+                                    <img src = {foto.url} className="img-fluid w-100 hover" style={{ height: "250px", objectFit: "cover" }} />
+                                </div>
+                            })
+                        }
+
+                    </div>
+                </>
+            }
             <hr />
             <div>
-                <h4 className='m-3'>Asistirán: </h4>
+                <h4 className='m-3'>Participantes: </h4>
 
                 <div className='row mt-3 ms-2'>
-                    {evento.participantes.map((participante, index) => {
+                    {evento?.participantes?.map((participante, index) => {
                         return <Link key={index} to={`/perfil/${participante.id}`} className='text-decoration-none'>
                             <div className='d-flex mb-3'>
 
@@ -193,25 +286,43 @@ function DetalleEvento() {
                     })}
                 </div>
             </div>
-            {eventoPasado &&
-                <>
-                    <hr />
-                    <div>
-                        <p>Sección para fotos</p>
-                    </div>
-                </>
-            }
-            <Modal show={show} onHide={handleClose}>
 
-                <Modal.Body>{mensaje}</Modal.Body>
+            <Modal show={show} onHide={handleClose}>
+                {tipoModal === "fotos"
+                    ? <>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Sube una imagen</Modal.Title>
+                        </Modal.Header>
+                        <Form>
+                            <Modal.Body>
+                                <Form.Control type="file" name='foto_perfil' onChange={handleFoto} accept="image/*" />
+                            </Modal.Body>
+                        </Form>
+                    </>
+                    : <Modal.Body>
+                        {mensaje}
+                    </Modal.Body>
+                }
+
                 <Modal.Footer>
-                    {tipoModal === "eliminar"
-                        ? <Button variant="secondary" onClick={handleEliminarButton}>
-                            Aceptar
-                        </Button>
-                        : <Button variant="secondary" onClick={handleClose}>
-                            Aceptar
-                        </Button>
+                    {
+                        tipoModal === "fotos"
+                            ? <div>
+                                <Button variant="secondary" className='me-2' onClick={enviarFoto}>
+                                    Guardar
+                                </Button>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Cancelar
+                                </Button>
+                            </div>
+                            : tipoModal === "eliminar"
+                                ? <Button variant="secondary" onClick={handleEliminarButton}>
+                                    Aceptar
+                                </Button>
+                                : <Button variant="secondary" onClick={handleClose}>
+                                    Aceptar
+                                </Button>
+
                     }
 
                 </Modal.Footer>
